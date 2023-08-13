1bdbd1a9cb6ceaca65df5ebc6e12bfb386f280eb
Parth Patel <58204750+john-patel@users.noreply.github.com>
2023-08-13 16:09:23 +0530

    Update urls.py

--- BudgetingApp/urls.py
+++ BudgetingApp/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('expenses/', include('expenses.urls')),
+    path('', include('expenses.urls')),
 ]
